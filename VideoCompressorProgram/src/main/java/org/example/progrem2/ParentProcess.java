6148ad1d3baf5c2335dca8f63ad1dfee5e5a6e51
suguru-3u <suguru.13579515@gmail.com>
2026-02-24 20:25:42 +0900

    ソケット通信プログラムの作成

--- VideoCompressorProgram/src/main/java/org/example/progrem2/ParentProcess.java
+++ VideoCompressorProgram/src/main/java/org/example/progrem2/ParentProcess.java
@@ -1,18 +1,20 @@
 package org.example.progrem2;
 
 import java.io.*;
-import java.util.Scanner;
+import java.net.Socket;
 
 public class ParentProcess {
     public static void main(String[] args) throws Exception {
-        try (BufferedWriter writer =
-                     new BufferedWriter(new FileWriter("/tmp/myfifo"))) {
+        try (Socket socket = new Socket("127.0.0.1", 8080)) {
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("なんか入力してください");
+            // サーバーからのメッセージ受信
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            writer.write(scanner.next());
-            writer.flush();
+            while (true) {
+                String input = reader.readLine();
+                System.out.println("[サーバーからのメッセージ受信]" + input);
+                if (input.equals("stop")) break;
+            }
         } catch (Exception e) {
             System.out.println(e);
         }
